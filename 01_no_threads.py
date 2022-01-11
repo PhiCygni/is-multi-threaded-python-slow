d76ba27b3080392b293c3a498820fbc49c0efaf4
BlueReverie <emileross@gmail.com>
2022-01-11 14:13:36 +0200

    Initial commit

---
+++ 01_no_threads.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3.8
+"""
+Test 1: The baseline test without threads or using
+any packages.
+
+"""
+import time
+
+
+def vThreadFunction():
+    """Function to do CPU-bound work.
+
+    Args:
+
+    Returns:
+
+    """
+
+    iResult = 0
+    for iCnt in range(50000000):
+        iResult += iCnt
+
+
+def vMain():
+    fTimePrefCountStart = time.perf_counter()
+
+    # Call the function eight times
+    for _ in range(8):
+        vThreadFunction()
+
+    fTimePrefCountEnd = time.perf_counter()
+    print(f"Delta time {fTimePrefCountEnd - fTimePrefCountStart} [s]")
+
+
+if __name__ == "__main__":
+    vMain()
